12f8f54e1be342b420cd27f18c123b9bbf22cb96
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2019-11-18 16:45:57 -0700

    minor improvements

--- results/fig_9_3.py
+++ results/fig_9_3.py
@@ -5,23 +5,30 @@ from utils.utils import get_feature
 
 
 def get_fig():
-    n_states = 1000
+    n_states = 500
     in_features = 1
-    out_features = 5
+    order = 15
+    out_features = (order + 1) ** in_features
     states = np.random.uniform(0, 1, (n_states, in_features))
 
     features = np.array(
         [
             get_feature(
                 states[i],
-                **{"order": 4, "features": "fourier", "in_features": out_features},
-                unit_norm=False
+                **{
+                    "order": order,
+                    "features": "fourier",
+                    "in_features": out_features,
+                    "v_min": 0,
+                    "v_max": n_states - 1,
+                },
+                unit_norm=True
             )
             for i in range(n_states)
         ]
     )
 
-    fig = plt.figure(figsize=(20, 4))
+    fig = plt.figure(figsize=(order * 3, 4))
     fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
     for i in range(1, out_features + 1):
