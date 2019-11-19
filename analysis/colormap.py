3d62e1b0c6b3792cca7051a800c047d5e42fbfad
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2019-11-18 18:31:50 -0700

    enhance

--- analysis/colormap.py
+++ analysis/colormap.py
@@ -4,3 +4,16 @@ colors = {"td": "red", "etd": "blue"}
 
 
 locs = {"td": 1, "etd": 2}
+
+lmbdas = {
+    0: "red",
+    0.4: "green",
+    0.8: "blue",
+    0.9: "black",
+    0.95: "magenta",
+    0.975: "cyan",
+    0.99: "violet",
+    1.0: "pink",
+}
+
+linestyles = {"td": "-", "etd": "--"}
