afd9a2693d6f7dc99824c90875878bee43eae8cb
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2019-11-19 11:46:01 -0700

    minor changes

--- scripts/run_puddle.py
+++ scripts/run_puddle.py
@@ -4,25 +4,26 @@ from pathlib import Path
 import gym
 import gym_puddle  # noqa f401
 import numpy as np
+from tqdm import tqdm
 
 from utils.utils import path_exists
 
 # TODO How to use TimeLimit's _elapsed_steps instead of own increment
 env = gym.make("PuddleWorld-v0")
 n_runs = 30
-# MAX_STEPS = 100
-steps = np.zeros(n_runs)
+n_episodes = 1000
+steps = np.zeros((n_runs, n_episodes))
+
 for i in range(n_runs):
-    out = env.reset()
-    done = False
-    while not done:
-        # for j in range(MAX_STEPS):
-        env.render(mode="rgb_array")
-        observation, reward, done, info = env.step(env.action_space.sample())
-        steps[i] += 1
-        print(f"\rRun: {i}, Steps: {steps[i]} done.", end="")
-        sys.stdout.flush()
-    path = Path(__file__).parents[1] / "results" / "PuddleWorld"
-    path_exists(path)
-    np.save(path / f"episode_steps.npy", steps)
+    for j in tqdm(range(n_episodes)):
+        env.reset()
+        done = False
+        while not done:
+            observation, reward, done, info = env.step(env.action_space.sample())
+            steps[i] += 1
+    print(f"\rRun: {i},\tAvg. Steps: {np.mean(steps[i]):.2f} done.", end="")
+    sys.stdout.flush()
+path = Path(__file__).parents[1] / "results" / "PuddleWorld"
+path_exists(path)
+np.save(path / f"episode_steps.npy", steps)
 env.close()
