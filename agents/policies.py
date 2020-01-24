e091b45a010d547fd62be24c3420cb775e3e59a3
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2020-01-24 16:55:53 -0700

    add policy semi-random-puddle

--- agents/policies.py
+++ agents/policies.py
@@ -3,5 +3,9 @@ def get_action_from_policy(name, rand_generator=None):
         left = 0
         right = 1
         return rand_generator.choice([left, right])
+    elif name == "semi-random-puddle":
+        north = 3
+        east = 1
+        return rand_generator.choice([north, east], p=[0.5, 0.5])
 
     raise Exception("Unexpected agent given")
