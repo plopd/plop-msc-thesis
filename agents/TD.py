80ca4169a6951e4a448c18c54908dfcda41fdc47
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2020-02-14 12:37:48 +0100

    refactor

--- agents/TD.py
+++ agents/TD.py
@@ -6,19 +6,30 @@ from representations.representations import get_representation
 
 
 class TD(BaseAgent):
+    def __init__(self):
+        super().__init__()
+        self.agent_info = None
+        self.step_size = None
+        self.discount_rate = None
+        self.trace_decay = None
+        self.policy = None
+        self.FR = None
+        self.weights = None
+        self.s_t = None
+        self.a_t = None
+
     def agent_init(self, agent_info):
         self.agent_info = agent_info
-        self.alpha = agent_info.get("step_size")
-        self.gamma = agent_info.get("discount_rate")
-        self.lmbda = agent_info.get("trace_decay")
+        self.step_size = agent_info.get("step_size")
+        self.discount_rate = agent_info.get("discount_rate")
+        self.trace_decay = agent_info.get("trace_decay")
         self.rand_generator = np.random.RandomState(agent_info.get("seed"))
         self.policy = agent_info.get("policy")
         self.FR = get_representation(
             name=agent_info.get("representations"), **agent_info
         )
         self.weights = np.zeros(self.FR.num_features)
-        self.s_t = None
-        self.a_t = None
+        self.eligibility = np.zeros(self.FR.num_features)
 
     def agent_start(self, observation):
         self.agent_cleanup()
@@ -56,12 +67,15 @@ class TD(BaseAgent):
         raise Exception("Unexpected agent message given.")
 
     def agent_cleanup(self):
-        self.eligibility = np.zeros_like(self.weights)
+        self.eligibility = np.zeros(self.FR.num_features)
 
     def learn(self, reward, current_state_feature, last_state_feature):
-        target = reward + self.gamma * np.dot(self.weights.T, current_state_feature)
+        target = reward + self.discount_rate * np.dot(
+            self.weights.T, current_state_feature
+        )
         pred = np.dot(self.weights.T, last_state_feature)
         self.eligibility = (
-            self.gamma * self.lmbda * self.eligibility + last_state_feature
+            self.discount_rate * self.trace_decay * self.eligibility
+            + last_state_feature
         )
-        self.weights += self.alpha * (target - pred) * self.eligibility
+        self.weights += self.step_size * (target - pred) * self.eligibility
