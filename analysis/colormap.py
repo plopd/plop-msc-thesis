28d4f0326e49d383bb420b25cae607ddc0b6d724
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2020-03-05 15:03:06 +0100

    update colormap to approx. match colormap in SB textbook.

--- analysis/colormap.py
+++ analysis/colormap.py
@@ -6,14 +6,14 @@ colors = {"TD": "crimson", "ETD": "blue"}
 locs = {"TD": 1, "ETD": 2}
 
 lmbdas = {
-    0: "red",
-    0.4: "green",
+    0: "crimson",
+    0.4: "lime",
     0.8: "blue",
     0.9: "black",
-    0.95: "magenta",
+    0.95: "violet",
     0.975: "cyan",
-    0.99: "violet",
-    1.0: "pink",
+    0.99: "purple",
+    1.0: "magenta",
 }
 
 linestyles = {"TD": "-.", "ETD": "--"}
