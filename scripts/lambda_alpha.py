acec43fae253ca14014615f9bffd4f8317d4b1c9
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2020-03-10 16:16:02 +0100

    generate lambda-stepsize pairs

---
+++ scripts/lambda_alpha.py
@@ -0,0 +1,38 @@
+import json
+
+import numpy as np
+
+lambdas = [0.0, 0.4, 0.8, 0.9, 0.95, 0.975, 0.99, 1]
+gammas = [0.99, 0.9995]
+alphas = [
+    [
+        np.arange(0, 1.8, 0.1),
+        np.arange(0, 1.8, 0.1),
+        np.arange(0, 0.99, 0.09),
+        np.arange(0, 0.65, 0.05),
+        np.arange(0, 0.53, 0.03),
+        np.arange(0, 0.32, 0.02),
+        np.arange(0, 0.11, 0.01),
+        np.arange(0, 0.044, 0.004),
+    ],
+    [
+        np.arange(0, 1.8, 0.1),
+        np.arange(0, 1.8, 0.1),
+        np.arange(0, 1.29, 0.09),
+        np.arange(0, 1.15, 0.05),
+        np.arange(0, 1.13, 0.03),
+        np.arange(0, 1.32, 0.02),
+        np.arange(0, 1.11, 0.01),
+        np.arange(0, 0.044, 0.004),
+    ],
+]
+
+
+lmbda2alpha = {}
+for g, gamma in enumerate(gammas):
+    lmbda2alpha[gamma] = {"discount_rate": [gamma], "lambdas_step_sizes": []}
+    for l, lmbda in enumerate(lambdas):
+        lmbda2alpha[gamma]["lambdas_step_sizes"].append(
+            {"trace_decay": [lmbda], "step_size": alphas[g][l].tolist()}
+        )
+print(json.dumps(lmbda2alpha, indent=4))
